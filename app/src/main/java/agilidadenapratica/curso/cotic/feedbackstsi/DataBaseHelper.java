07486285cffdba2338b87ec289b643b40b5c25bb
Armando <armandoalanalmeida@gmail.com>
2017-09-26 08:12:31 -0300

    primeiro commit

---
+++ app/src/main/java/agilidadenapratica/curso/cotic/feedbackstsi/DataBaseHelper.java
@@ -0,0 +1,103 @@
+package agilidadenapratica.curso.cotic.feedbackstsi;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by cess on 21/09/17.
+ */
+
+public class DataBaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE = "app";
+    private static int VERSAO = 1;
+
+    public DataBaseHelper(Context context) {
+        super(context, DATABASE, null, VERSAO);
+    }
+
+    @Override
+    //Cria a tabela Palestra e a Tabela Avaliação
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS palestra (_id INTEGER PRIMARY KEY AUTOINCREMENT, titulo VARCHAR, tipo INT(1))");
+        db.execSQL("CREATE TABLE IF NOT EXISTS avaliacao " +
+                "(_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "avaliacao INT(1)," +
+                "sugestao VARCHAR," +
+                "palestra_id INT (3) NOT NULL, " +
+                "FOREIGN KEY(palestra_id) REFERENCES palestra (_id))");
+    }
+
+    //Insere uma nova palestra
+    public void inserirPalestra(String titulo, int tipo){
+        ContentValues values = new ContentValues();
+        values.put("titulo", titulo);
+        values.put("tipo", tipo);
+        getWritableDatabase().insert("palestra", null, values);
+    }
+
+    // Insere uma nova Avaliação
+    public void inserirAvaliacao (int avaliacao, String sugestao, int palestraId ){
+        ContentValues values = new ContentValues();
+        values.put("avaliacao", avaliacao);
+        values.put("sugestao", sugestao);
+        values.put("palestra_id",palestraId);
+        getWritableDatabase().insert("avaliacao", null, values);
+
+    }
+    // Pega TODOS os registros de uma tabela
+    public Cursor carregaDados(String[] campos, String table){
+        Cursor cursor;
+        SQLiteDatabase db = this.getReadableDatabase();
+        cursor = db.query(table, campos, null, null, null, null, null, null);
+        if(cursor!=null){
+            cursor.moveToFirst();
+        }
+        db.close();
+        return cursor;
+    }
+
+    // Apaga um registro de uma tabela conforme a condição where
+    public void deleteRegistro(String nomeTabela, String condicaoWhere){
+        getWritableDatabase().delete(nomeTabela, condicaoWhere, null);
+    }
+
+    //Carrega dados por id
+    public Cursor carregaDadoById(int id, String[] campos, String table){
+        Cursor cursor;
+        String where = "_id =" + id;
+        SQLiteDatabase db = this.getReadableDatabase();
+        cursor = db.query(table,campos,where, null, null, null, null, null);
+
+        if(cursor!=null){
+            cursor.moveToFirst();
+        }
+        db.close();
+        return cursor;
+    }
+
+    // Carrega avaliação de uma palestra
+    public Cursor carregaAvaliacaoByIdPalestra(int idPalestra, String[] campos){
+        Cursor cursor;
+        String where = "palestra_id =" + idPalestra;
+        SQLiteDatabase db = this.getReadableDatabase();
+        cursor = db.query("avaliacao", campos, where, null, null, null, null, null);
+        cursor.moveToFirst();
+        if(cursor != null){
+            cursor.moveToFirst();
+        }
+        return cursor;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXIST palestra");
+        db.execSQL("DROP TABLE IF EXIST avaliacao");
+        onCreate(db);
+    }
+
+}
+
---
+++ app/src/main/java/agilidadenapratica/curso/cotic/feedbackstsi/FeedBackHelper.java
@@ -0,0 +1,89 @@
+package agilidadenapratica.curso.cotic.feedbackstsi;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
+/**
+ * Created by armando on 25/09/17.
+ */
+
+
+public class FeedBackHelper extends Activity{
+    private DataBaseHelper helper;
+
+    public void showListPalestras(final Context contextoAtual, int[] idViews, ListView listarPalestra, final Class activityDestino) {
+
+        final Cursor cursor = getCamposTabelaPalestra(contextoAtual);
+        String[] nomeCampos =  {"_id","titulo"};
+        SimpleCursorAdapter adapterPalestras = new SimpleCursorAdapter(
+                contextoAtual,
+                R.layout.content_main,
+                cursor,
+                nomeCampos,
+                idViews,
+                0
+        );
+
+        listarPalestra.setAdapter(adapterPalestras);
+        listarPalestra.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int codigo;
+                cursor.moveToPosition(position);
+                Intent intent = new Intent(contextoAtual , activityDestino);
+                codigo = cursor.getInt(cursor.getColumnIndexOrThrow("_id"));
+                intent.putExtra("id", codigo);
+                contextoAtual.startActivity(intent);
+            }
+        });
+    }
+
+
+    public void createDialogExcluirPalestra(AlertDialog.Builder dialog, final Context contextoAtual, final int idPalestra){
+            helper = getContextHelper(contextoAtual);
+            dialog = new AlertDialog.Builder(contextoAtual);
+            dialog.setTitle("Deletar Palestra");
+            dialog.setMessage("Você tem certeza que deseja deletar palestra ?");
+
+            dialog.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            dialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    helper.deleteRegistro("palestra", "_id = " + idPalestra);
+                    contextoAtual.startActivity(new Intent(contextoAtual, MainActivity.class));
+                    Toast.makeText(contextoAtual, "Palestra apagada", Toast.LENGTH_SHORT).show();
+                }
+            });
+            AlertDialog alert = dialog.create();
+            alert.show();
+        }
+
+    public Cursor getCamposTabelaPalestra(Context context){
+        helper = getContextHelper(context);
+        String[] campos =  {"_id","titulo", "tipo"};
+        String table = "palestra";
+        final Cursor cursor = helper.carregaDados(campos, table);
+        return cursor;
+
+    }
+
+    public DataBaseHelper getContextHelper(Context context){
+        helper = new DataBaseHelper(context);
+        return helper;
+    }
+
+}
